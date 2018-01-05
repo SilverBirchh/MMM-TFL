49fb0e140205e2fe32c2798a03bccc75dc7e4292
SilverBirchh <bradleybirch09@yahoo.co.uk>
2018-01-05 11:48:15 +0000

    shows lines one time

--- MMM-TFL.js
+++ MMM-TFL.js
@@ -1,49 +1,62 @@
 Module.register("MMM-TFL", {
+  // Default module config.
+  defaults: {
+    lines: "all"
+  },
 
-    // Default module config.
-    defaults: {
-        text: "Hello World!"
-    },
+  start: function() {
+    this.mySpecialProperty = "So much wow!";
+    Log.log(this.name + " is started!");
+  },
+  getDom: function() {
+    Log.info("GETTING THE DOM");
+    const ul = document.createElement("ul");
+    const lines = this.config.lines;
 
-    start: function() {
-        this.mySpecialProperty = "So much wow!";
-        Log.log(this.name + ' is started!');
-    },
-    getDom: function() {
-        Log.error(GETTING THE DOM);
-        var wrapper = document.createElement("ul");
-        const lines = 'all';
-        document.getElementById("lines").innerHTML = "";
-        const ul = document.getElementById("lines");
+    fetch("https://api.tfl.gov.uk/line/mode/tube/status" /*, {}*/)
+      .then(result => result.json())
+      .then(result => {
+        if (lines !== "all") {
+          result = result.filter(line => lines.includes(line.id));
+        }
+        result.forEach(line => {
+          const li = document.createElement("li");
 
-        fetch("https://api.tfl.gov.uk/line/mode/tube/status" /*, {}*/ )
-            .then(result => result.json())
-            .then(result => {
-                if (lines !== 'all') {
-                    result = result.filter(line => lines.includes(line.id))
-                }
-                result.forEach(line => {
-                    const li = document.createElement("li");
+          const lineName = document.createElement("span");
+          lineName.innerHTML = line.name;
+          lineName.setAttribute("class", line.id);
+          lineName.classList.add("line");
 
-                    const lineName = document.createElement("span");
-                    lineName.innerHTML = line.name;
-                    lineName.setAttribute("class", line.id);
-                    lineName.classList.add("line");
+          const status = document.createElement("span");
+          status.innerHTML = this.getLineStatusText(line);
+          status.setAttribute("class", this.getLineStatusClass(line));
+          status.classList.add("status");
 
-                    const status = document.createElement("span");
-                    status.innerHTML = getLineStatusText(line);
-                    status.setAttribute("class", getLineStatusClass(line));
-                    status.classList.add("status");
+          li.appendChild(lineName);
+          li.appendChild(status);
+          ul.appendChild(li);
+        });
+      });
+    return ul;
+  },
 
-                    li.appendChild(lineName)
-                    li.appendChild(status);
-                    ul.appendChild(li);
-                });
-            });
-        return wrapper;
-    }
-
-  getStyles: function () {
+  getStyles: function() {
     return ["MMM-TFL.css"];
   },
+
+  getLineStatusText: function(line) {
+    const reason = line.lineStatuses[0].reason
+      ? line.lineStatuses[0].reason.subString(
+          0,
+          line.lineStatuses[0].reason.indexOf(":")
+        )
+      : "";
+    return `${line.lineStatuses[0].statusSeverityDescription} ${reason}`;
+  },
+
+  getLineStatusClass: function(line) {
+    return line.lineStatuses[0].statusSeverityDescription
+      .toLowerCase()
+      .replace(" ", "-");
+  }
 });
